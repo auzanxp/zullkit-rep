92c6f065a7bfda73f001b3ff0f0a1b38c3ffc3a8
engineering <istikhoriauzan@gmail.com>
2022-11-15 22:33:01 +0700

    migration html final sesi 10

--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -1,22 +1,26 @@
 <script setup>
-import { ref } from 'vue';
-import ItemCard from './../ItemCard.vue'
+import { ref } from "vue";
+import ItemCard from "./../ItemCard.vue";
 
 const items = ref([
-    { id: 1, title: 'Mobile UI Kit', count: 731, image: 'items-1.jpg' },
-    { id: 2, title: 'Mobile UI Kit', count: 731, image: 'items-2.jpg' },
-    { id: 3, title: 'Mobile UI Kit', count: 731, image: 'items-3.jpg' }
-])
-
-
+  { id: 1, title: "Mobile UI Kit", count: 731, image: "items-1.jpg" },
+  { id: 2, title: "Mobile UI Kit", count: 731, image: "items-2.jpg" },
+  { id: 3, title: "Mobile UI Kit", count: 731, image: "items-3.jpg" },
+]);
 </script>
 
 <template>
-    <div class="container px-4 mx-auto my-16 md:px-12">
-        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <ItemCard v-for="item in items" :key="item.id" :title="item.title" :count="item.count"
-                :image="item.image" />
-        </div>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <ItemCard
+        v-for="item in items"
+        :key="item.id"
+        :id="item.id"
+        :title="item.title"
+        :count="item.count"
+        :image="item.image"
+      />
     </div>
+  </div>
 </template>
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,6 +2,7 @@
 import Logo from './Logo.vue'
 import UserInfo from './UserInfo.vue';
 import NavigationLink from './NavigationLink.vue'
+import AuthButton from './AuthButton.vue'
 
 </script>
 
@@ -9,7 +10,8 @@ import NavigationLink from './NavigationLink.vue'
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
         <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
             <Logo />
-            <UserInfo />
+            <!-- <UserInfo /> -->
+            <AuthButton/>
             <NavigationLink />
         </div>
     </nav>
